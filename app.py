4f5d730c3e3fc5363e8392459dbe75192e641005
audun-web <audun@midtmeck.com>
2025-10-20 14:34:07 +0200

    Init + simple nav bar in base.html and style.css

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/meny')
+def meny():
+    return render_template('meny.html')
+
+@app.route('/varer')
+def varer():
+    return render_template('varer.html')
+
+@app.route('/kontakt')
+def kontakt():
+    return render_template('kontakt.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
